a9c90ce0e08b360c6d6fbad4c5101d5fd1076593
Brijesh Dubey <brijesh.kdubey@parth.com>
2023-05-30 22:21:00 +0530

    Adavanced gcd's problem explanation added via comment

--- src/AdvancedGCD.java
+++ src/AdvancedGCD.java
@@ -1,6 +1,33 @@
 import java.io.*;
 
-
+/**
+ * Varun explained its friend Sanchit the algorithm of Euclides to calculate the GCD of two numbers. Then Sanchit implements the algorithm
+ * int gcd(int a, int b)
+ * {
+ *
+ *     if (b==0)
+ *     return a;
+ *     else
+ *     return gcd(b,a%b);
+ * }
+ * and challenges to Varun to calculate gcd of two integers, one is a little integer and another integer can have 10^4 digits.
+ * Your task is to help Varun an efficient code for the challenge of Sanchit
+ *
+ *
+ * Input Format
+ *
+ * The first line of input will contain T(number of the test case), each test case follows as.
+ * Each test case consists of two number A and B.
+ *
+ * 1 <= T <= 100
+ * 1 <= A <= 4*10^5
+ * 1 <= |B| <= 10^4
+ * where |B| is the length of the integer B
+ *
+ * Output Format:
+ * Print for each pair (A,B) in the input one integer representing the GCD of A and B.
+ */
+/
 
 public class AdvancedGCD {
 
